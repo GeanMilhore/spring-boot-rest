8cbb859fd3e002ced81387ff8f711e5c772aadfd
Gean Milhore <gean.milhore@gmail.com>
2023-01-08 19:24:01 -0300

    aula 13 - adicao de roles em endpoints diferentes

--- src/main/java/br/com/alura/forum/config/security/SecurityConfiguration.java
+++ src/main/java/br/com/alura/forum/config/security/SecurityConfiguration.java
@@ -49,6 +49,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 		.antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
 		.antMatchers(HttpMethod.POST, "/auth").permitAll()
 		.antMatchers(HttpMethod.GET, "/actuator/**").permitAll()
+		.antMatchers(HttpMethod.DELETE, "/topicos/*").hasRole("MODERADOR")
 		.anyRequest().authenticated()
 		.and().csrf().disable()
 		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
@@ -59,6 +60,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	// Configurações de Recursos Estaticos(js, css, etc.)
 	@Override
 	public void configure(WebSecurity web) throws Exception {
-		web.ignoring().antMatchers("/**.html", "/v2/api-docs", "/webjars/**","/configuration/**", "/swagger-resources/**");
+		web.ignoring().antMatchers("/h2-console","/h2-console/**","/**.html", "/v2/api-docs", "/webjars/**","/configuration/**", "/swagger-resources/**");
 	}
 }
